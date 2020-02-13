e70b5d94a9d0fe5c894cff4f6bdf4d68ae93a57a
Zhenyi He <snowymohehe@gmail.com>
2020-02-13 14:50:00 -0500

    :beer: press trigger to recenter the entire panel, it is not always in front of the camera anymore. Also, we can see controllers now, but I felt it is not necessary and easy to locate the keyboard via controllers.

---
+++ tobiieye/GazeTyping/Assets/Scripts/ControllerHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.XR.Examples;
+using UnityEngine;
+
+// take care of the vive controller action
+
+public class ControllerHandler : MonoBehaviour
+{
+
+    public Transform TypingSection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TypingSection.position = Camera.main.gameObject.transform.position;
+        TypingSection.rotation = Camera.main.gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ControllerManager.Instance.GetButtonPressDown(ControllerButton.Trigger))
+        {
+            // reset the input area, put it in front of the current camera
+            TypingSection.position = Camera.main.gameObject.transform.position;
+            TypingSection.rotation = Camera.main.gameObject.transform.rotation;
+        }
+    }
+}
