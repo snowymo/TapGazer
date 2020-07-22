277f47322388252885d741c6f89e4899132a965d
Zhenyi He <snowymohehe@gmail.com>
2020-07-22 10:04:23 -0700

    :bee: generate wordfreq file for Ken

--- freq/generateWordWithFreq.py
+++ freq/generateWordWithFreq.py
@@ -16,5 +16,18 @@ def generateWordFreq(filename):
     with open(filename.split(".")[0] + "-freq.json", 'w') as outfile:
         json.dump(wordFreqPair, outfile)
 
+def generateWordFreqForKen(filename):
+    phrasesDict = []
+    with open(filename, encoding="utf-8") as f:
+        phrasesDict = f.read().splitlines()
+
+    with open(filename.split(".")[0] + "-freq-ken.js", 'w') as outfile:
+        outfile.write("let words = {\n")
+        for item in phrasesDict:
+            item = re.sub('\s+', '', item)
+            outfile.write("\"" + item + "\":" + str("%.2f" % zipf_frequency(item, "en", "large")) + ",\n")
+        outfile.write("};")
+
 if __name__ == '__main__':
-    generateWordFreq("top40k.txt")
+    # generateWordFreq("top40k.txt")
+    generateWordFreqForKen("top40k.txt")
